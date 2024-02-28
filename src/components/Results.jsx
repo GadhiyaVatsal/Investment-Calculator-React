89614765efeac7d58cad171966aa63a3bb480bf0
GadhiyaVatsal <gadhiya.vatsal007@gmail.com>
2024-02-27 20:51:32 -0500

    bug solved

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,4 @@
-import {
-  calculateInvestmentResults,
-  formatter,
-} from "../utils/calculateInvestmentResults";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ userInput }) {
   const resultData = calculateInvestmentResults(userInput);
@@ -11,34 +8,37 @@ export default function Results({ userInput }) {
     resultData[0].annualInvestment;
 
   return (
-    <table id="results">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {resultData.map((yearData) => {
-          const totalInterest =
-              yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
-            
+    <div className="center-table">
+      <table id="results">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultData.map((yearData) => {
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year;
+
             const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
 
-          yearData.return(
-            <tr key={year}>
-              <td>{yearData.year}</td>
-              <td>{formatter.format(yearData.valueEndOfYear)}</td>
-              <td>{formatter.format(yearData.interest)}</td>
-              <td>{formatter.format(totalInterest)}</td>
-              <td>{formatter.format(totalAmountInvested)}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr key={yearData.year}>
+                <td className="td">{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
